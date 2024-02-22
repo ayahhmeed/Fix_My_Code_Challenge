e5df6324bf6621b51e21febfb76c41ab7e63d845
ayahhmeed <ayahhmeed@gmail.com>
2024-02-21 19:16:03 -0800

    debug3

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -31,16 +31,18 @@ class User():
 
     @password.setter
     def password(self, pwd):
-    """
-    Password setter:
-    - `None` if `pwd` is `None`
-    - `None` if `pwd` is not a string
-    - Hash `pwd` in MD5 before assigning to `__password`
-    """
-    if pwd is None or type(pwd) is not str:
-        self.__password = None
-    else:
-        self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
+        """
+        Password setter:
+        - `None` if `pwd` is `None`
+        - `None` if `pwd` is not a string
+        - Hash `pwd` in MD5 before assigning to `__password`
+        """
+
+    
+        if pwd is None or type(pwd) is not str:
+            self.__password = None
+        else:
+            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd):
         """
